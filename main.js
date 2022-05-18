3283cae53331825204df1a4f118f1b7cf415f684
christopher-k-c <chriscareyk@gmail.com>
2022-05-18 16:23:13 +0100

    Wednesday Afternoon Changes

--- main.js
+++ main.js
@@ -1,44 +1,50 @@
-
-
-
-
-// Add event handler for clicking buttons 
-
-// // * Store the numeric value of each unique button and push it two a array
-
-// let div = document.querySelectorAll(".box");
-
+// The main container where the divs are created
 let cont = document.querySelector(".container");
 
+// All winning positions 
 let winningP = [
 
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    ["0","1","2"],
+    ["3","4","5"],
+    ["6","7","8"],
+    ["0","3","6"],
+    ["1","4","7"],
+    ["2","5","8"],
+    ["0","4","8"],
+    ["2","4","6"]
 
 ]
 
-// let currentPlayer = x;
+// Tracks round wins
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
 
+// No match 
+
+let turn = 0;
+
+
+// Tracks the computers choice
+let computerGo = 0;
+
+
+// The computers move options, each time the user selects a box the computerOptions removes the number the user has selected
+let computerOptions = [0,1,2,3,4,5,6,7,8];
+
+
+// Stores the human and computers whilst the game is underway  
 let humanArr = [];
+let computerArr = [];
 
-// Game Scores
 
-let compScore = [];
-
-// Arrays 
-
+// Defines array width and height  
 let width = 3;
-
 let cellCount = width * width;
 
-// Function that creates grid of divs
 
+
+// Function that creates grid of divs
 function createGrid(){
 
     for (let index = 0; index < cellCount; index++) {
@@ -51,34 +57,15 @@ function createGrid(){
     
         cont.appendChild(div)
         
-        // cells.push(div);
-
         div.innerText = index;
 
     }
 
 }
-
 createGrid();
 
-// JQuery that finds index of each .box when clicked and then returns it
-// let flipCoin = Math.floor(Math.random() * 2)
-// let computerUser = 1;
-// let humanUser = 0;
 
-// Tracks round wins
-let playerScore = [];
-let computerScore = [];
 
-// Tracks turns
-let playerGo = 0;
-let computerGo = 0;
-
-// The computers move options, each time the user selects a box the computerOptions removes the number the user has selected
-let computerOptions = [0,1,2,3,4,5,6,7,8];
-
-// While player score is < 3 carry on 
-// If player score is equal to 3 they win the round 
 
 $(".box").click(function(){
 
@@ -94,142 +81,254 @@ $(".box").click(function(){
     // This colors the text of the selected box
     $(this).css({color: 'red'});
 
+    // Adds the X from the user input into the div currently selected 
     $(this).text("X");
 
-
-    function removeText(){
-        $(".box").remove("X");
-        
-    }
-
-
-
-
-
-    //  let xElement = document.createElement("img");
-    //  xElement.setAttribute("src", "https://via.placeholder.com/150");
-    //  document.getElementById("code").appendChild(xElement);
-
-    // let userSelection = document.getElementById(code);
-    // console.log(userSelection)
-
+    // Logs the button you have selected 
     console.log(`You have clicked on ${$(this).attr('id')}`)
-    console.log(computerOptions)
+
+    // Removes the users choice from the computers options 
+    removeUserChoice(code);
+    
+    // The computer makes it's choice
+    addComputerChoice();
+
+    // The computer removes it's previous choice from it's list of options
+    removeComputerChoice();
+        
+    // Checks if theres a winning set of inputs
+    checkGameScore();
+  
+});
+
+function removeUserChoice(id){
 
     // This loop removes the Users choice from the computerOptions array 
     // This loop checks the computerOptions variable and splices the the number of 
     // the index of the box that the player most recently clicked on
-    
     for (let index = 0; index < computerOptions.length; index++) {
         
-        if(computerOptions[index] == code){
+        if(computerOptions[index] == id){
             
             computerOptions.splice(index, 1);
-            console.log(computerOptions)
+            
 
         }
     }
 
+}
+
+function addComputerChoice(){
+
+
+   
+    // Decides which number the computer can pick 
     computerGo = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+
+    // Adds that choice to the computerArr array
+    computerArr.push(computerGo);
+
+    // Sorts that array in ascending order 
+    computerArr.sort(function(a, b){return a-b});
+
+    turn++
+    
+    
+
+}
+
+function removeComputerChoice(){
 
     // This loop removes the Computers choice (computerGo) from the computerOptions array 
     // This loop checks the computerOptions variable and splices the the number of the 
     // index of the box that the computerGo variable selected
-    
-
     for (let index = 0; index < computerOptions.length; index++) {
         
         if(computerOptions[index] == computerGo){
             
             computerOptions.splice(index, 1);
-            console.log(computerOptions)
-
+            
         }
     }
     
-
-
     console.log("The Computer picks " + computerGo + "!")
 
-    let x = $(`#${computerGo}`).text("O");
+    $(`#${computerGo}`).text("O");
+}
 
-    console.log(x)
+function checkGameScore(){
 
-    console.log("computerOptions", computerOptions)
+    // var containsAll = false;
+    // var terminated = false;
 
-
-
-    // If neither of us score a winning position we need to default end game as a tie 
-
-    // A tie will take place when all 9 boxes have been filled 
-
-    // if ()
-   
-
-    // Conditional For finding a winning match 
-    if(humanArr.equals([0,1,2]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-    
-    } else if(humanArr.equals([3,4,5]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([6,7,8]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([0,3,6]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([1,4,7]) == true){
-       
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([2,5,8]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([0,4,8]) == true){
-
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
-
-    } else if(humanArr.equals([2,4,6]) == true){
+    // winningP.forEach(winC => {
+    //     // console.log(winC)
+    //     // console.log(humanArr)
+    //         containsAll = winC.every(element => {
+    //         var result = humanArr.includes(element);
+    //         return result;
         
-        playerScore++;
-        console.log(playerScore);
-        console.log(`${humanArr} is a winning position`);
-        return resetGame();
+    //     }); 
+
+    //     console.log(containsAll)
+
+    //     if(containsAll == true){
+    //         terminated = true;
+    //         // console.log("Winning Combination Matched")
+    //     } else {
+    //         // console.log("Winning combo not matched")
+    //     }
+    // });
+    // console.log(terminated)
+
+    
+
+    if((humanArr.equals([0,1,2]) == true) || (computerArr.equals([0,1,2]) == true)){
+
+
+        if(computerArr.equals([0,1,2]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+        
+    }
+     else if((humanArr.equals([3,4,5]) == true) || (computerArr.equals([3,4,5]) == true)){
+
+
+
+        if(computerArr.equals([3,4,5]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([6,7,8]) == true) || (computerArr.equals([6,7,8]) == true)){
+
+        if(computerArr.equals([6,7,8]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([0,3,6]) == true) || (computerArr.equals([0,3,6]) == true)){
+
+        if(computerArr.equals([0,3,6]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([1,4,7]) == true) || (computerArr.equals([1,4,7]) == true)){
+       
+        if(computerArr.equals([1,4,7]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([2,5,8]) == true) || (computerArr.equals([2,5,8]) == true)){
+
+        if(computerArr.equals([2,5,8]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([0,4,8]) == true) || (computerArr.equals([0,4,8]) == true)){
+
+        if(computerArr.equals([0,4,8]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
+
+    } else if((humanArr.equals([2,4,6]) == true) || (computerArr.equals([2,4,6]) == true)){
+        
+        if(computerArr.equals([2,4,6]) == true) {
+
+            computerScore++
+            console.log(`The computer wins!`)
+
+        } else {
+
+            playerScore++
+            console.log(`The human wins!`)
+
+        }
+        checkScore();
+        resetGame();
 
     } else {
+        //My clicks are applying one to noMatch, it also needs to be used by the computer
+        turn++
+        console.log(turn)
+    }
 
-        console.log("false")
-    
-    };
-    
-});
+    }
+
+    // else if number of turns is equal to 8 log a draw.....
+    // else if( turn >= 8 ) { draw++ console.log(" It's a draw")}
+    // document.getElementById("draw").innerHTML = `${draw}`
+
+
+
 
 
 // Compares Arrays
@@ -267,13 +366,28 @@ Object.defineProperty(Array.prototype, "equals", {enumerable: false});
 
 
 
+function checkScore(){
 
+    if(playerScore >= 3){
+        playerScore = 0;
+        console.log("Player Wins the game")
+    } else if (computerScore >= 3) {
+        computerScore = 0;
+        console.log("Computer Wins the game")
+    } else {
+        document.getElementById("P1").innerHTML = `${computerScore}`;
+        document.getElementById("P2").innerHTML = `${playerScore}`;
+        console.log("Play on!")
+    }
+
+}
 
 
 
 // Reset Game Function 
 
 function resetGame(){
+    
 
     // https://bit.ly/3NixlSR
 
@@ -290,14 +404,60 @@ function resetGame(){
 
     });
 
+    computerArr.forEach(element => {
+
+        while(computerArr.length > 0) {
+
+            computerArr.pop(element)
+            console.log(computerArr)
+            
+
+        }
+        
+
+    });
+
+
+    // Removes all elements content - Sorry i know it;s terrible want to refactor
+
+    document.getElementById("0").innerHTML = "";
+    document.getElementById("1").innerHTML = "";
+    document.getElementById("2").innerHTML = "";
+    document.getElementById("3").innerHTML = "";
+    document.getElementById("4").innerHTML = "";
+    document.getElementById("5").innerHTML = "";
+    document.getElementById("6").innerHTML = "";
+    document.getElementById("7").innerHTML = "";
+    document.getElementById("8").innerHTML = "";
+
+
+    // Resets computerOption array to the full list of indexes 
+    computerOptions = [0,1,2,3,4,5,6,7,8];
+
+    noMatch = 0;
+
+    
+
+
+
+
     
 
 }
 
+// const arr1 = ['pizza', 'cola', 'pepsi'];
+// const arr2 = ['pizza', 'cake', 'cola'];
+
+// const containsAll = arr1.every(element => {
+//   return arr2.includes(element);
+// });
+
+// console.log(containsAll);
 
 
 
 
 
 
-// PLanning on using neon text 
+
+
